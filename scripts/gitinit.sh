58d8e7688c782ae79c58ece673590a8807967f6c
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2025-06-03 08:38:31 +0000

    🩹 Patch indentation

--- scripts/gitinit.sh
+++ scripts/gitinit.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 cat << EOF > "$HOME/.gitconfig.local"
-; I use ghcli to manage my git credentials
+; I use gh-cli to manage my git credentials
 [credential "https://github.com"]
-	helper =
-	helper = !/usr/bin/gh auth git-credential
+    helper =
+    helper = !/usr/bin/gh auth git-credential
 [credential "https://gist.github.com"]
-	helper =
+    helper =
     helper = !/usr/bin/gh auth git-credential
 [user]
     ; signingkey = <path to your signing key>
-	; email = <your email>
-	; name = <your name>
+    ; email = <your email>
+    ; name = <your name>
 EOF
