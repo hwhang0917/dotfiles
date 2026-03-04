8dfd2aea7bbc88c02b5d005680a26cdf5185d463
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2026-03-04 14:06:08 +0900

    fix(glzr): zebar build error

--- glzr/.glzr/zebar/starter/vite.config.ts
+++ glzr/.glzr/zebar/starter/vite.config.ts
@@ -1,8 +1,10 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [react(), viteSingleFile()],
   build: {
     outDir: "dist",
